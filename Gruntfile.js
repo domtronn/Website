024c826ed137c9bf1417108aa468c9406eec3fc9
Dom Charlesworth <dgc336@gmail.com>
2015-06-01 21:12:32 +0100

    Reformatted

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,73 @@
 /*global module:false*/
 
-var env = require('./.env.js'), 
-		s3 = require('./.s3.js');
+var env = require("./.env.js"),
+		s3 = require("./.s3.js");
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // Project configuration.
   grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		clean: [ "dist/" ], 
-		
+		pkg: grunt.file.readJSON("package.json"),
+		clean: [ "dist/" ],
+
 		aws_s3: {
 			release: {
 				options: {
-					accessKeyId: s3.accessKeyId,          
-          secretAccessKey: s3.secretAccessKey,
-          bucket: env.s3.bucket,    
-          region: env.s3.region,        
-          sslEnabled: false
+					accessKeyId: s3.accessKeyId,
+					secretAccessKey: s3.secretAccessKey,
+					bucket: env.s3.bucket,
+					region: env.s3.region,
+					sslEnabled: false
 				},
 				files: [
-          {
-            expand: true, 
-            dest: '.', 
-            cwd: 'dist/', 
-            src: ['**'], 
-            action: 'upload', 
-            differential: true
-          }
-        ]
+					{
+						expand: true,
+						dest: ".",
+						cwd: "dist/",
+						src: ["**"],
+						action: "upload",
+						differential: true
+					}
+				]
 			}
 		},
 
 		push: {
-			options: { tagName: '%VERSION%' }
+			options: { tagName: "%VERSION%" }
 		},
 
 		shell: {
 			build: {
 				command: function (project) {
-					return 'cd ../' + project + '; grunt build;';
+					return "cd ../" + project + "; grunt build;";
 				}
 			}
-		}, 
-		
-		'merge-copy': {
+		},
+
+		"merge-copy": {
 			release: {
 				options: {
-					destination: 'dist/',
-					directories: [ '../main/dist/', '../lab/dist/' ]
+					destination: "dist/",
+					directories: [ "../main/dist/", "../lab/dist/" ]
 				}
 			}
 		}
-		
+
   });
 
   // These plugins provide necessary tasks.
 
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-aws-s3');
-	grunt.loadNpmTasks('grunt-merge-copy');
-	grunt.loadNpmTasks('grunt-push-release');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	
+	grunt.loadNpmTasks("grunt-shell");
+	grunt.loadNpmTasks("grunt-aws-s3");
+	grunt.loadNpmTasks("grunt-merge-copy");
+	grunt.loadNpmTasks("grunt-push-release");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+
   // Default task.
 
-	grunt.registerTask('deploy:setup', ['clean', 'shell:build:main', 'shell:build:lab',  'merge-copy:release']);
-	
-	grunt.registerTask('deploy:major', ['deploy:setup', 'push:major', 'aws_s3:release']);
-	grunt.registerTask('deploy:minor', ['deploy:setup', 'push:minor', 'aws_s3:release']);
-	
+	grunt.registerTask("deploy:setup", ["clean", "shell:build:main", "shell:build:lab",  "merge-copy:release"]);
+
+	grunt.registerTask("deploy:major", ["deploy:setup", "push:major", "aws_s3:release"]);
+	grunt.registerTask("deploy:minor", ["deploy:setup", "push:minor", "aws_s3:release"]);
+
 };
